cd3a2025fa38c5aad384fc1ca923be527335ee80
Matthieu Moinvaziri <mmoinvaziri@localhost.localdomain>
2019-09-14 22:42:35 +0200

    Updated cmake

--- Library/openApp/Renderer.cmake
+++ Library/openApp/Renderer.cmake
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.0.0)
 project(openApp)
 
-# This file need variables 'oAPath' and 'oARendererTarget' to be set !
+# This file need variables 'oALibraryDir' and 'oARendererTarget' to be set !
 
 include(openApp.cmake)
 
--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 rm -rf Build
 rm -rf BuildTests
-rm -rf Library/libopenApp.*
+rm -rf Library/libopen*.*
